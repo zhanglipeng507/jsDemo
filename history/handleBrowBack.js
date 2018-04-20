54e4fb3125fc0dbd28164170d4110224caf9a7a0
zhanglipeng <zhanglipeng@socian.com.cn>
2018-04-20 10:27:37 +0800

    浏览器返回 监听

---
+++ history/handleBrowBack.js
@@ -0,0 +1,19 @@
+/*
+ * 功能： 监听浏览器后退按钮 直接返回第num个页面。
+ *
+ * 使用： 在需要实现该功能的页面 页面加载完成后执行一次该方法！
+ *
+ * @param {Number} num 返回第num个页面
+ *      示例 ： 返回第一个页面 入参num为 1 返回第二个页面 入参num为 2 ...
+ * @return undefind
+ * */
+function handleBrowBack(num){
+
+    window.addEventListener("popstate",handlePopstate, false);
+    window.history.pushState({}, "", "");
+
+    function handlePopstate (){
+        var len = window.history.length;
+        history.go(-(len - 1 - num));
+    }
+}
